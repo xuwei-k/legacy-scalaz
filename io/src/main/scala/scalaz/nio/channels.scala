242893d789385f7c21db36998cfcdd6cb368072d
jsuereth <Joshua.Suereth@gmail.com>
2011-04-27 22:00:20 -0400

    NIO now working

--- io/src/main/scala/scalaz/nio/channels.scala
+++ io/src/main/scala/scalaz/nio/channels.scala
@@ -11,14 +11,13 @@ import iteratees._
  * java.nio.Channels.
  */
 object Channels {
-  // TODO - Temprrary for testing:
-  def fileInputChannel(file : java.io.File)(implicit m : Monad[IO]) : IO[JByteChannel] =
-    m.pure(file).map(new java.io.FileInputStream(_)).bracket(
-        i => {i.close(); m.pure(())}
-      )(i => m.pure(i.getChannel()))
 
-
-  def enumByteChannel[A](channel : IO[JByteChannel], buf : ByteBuffer) = new Enumerator[ByteBuffer, IO] {
+  /**
+   * Construct an enumerator of nio ByteChannel's that uses the given buffer.
+   * @param channel  The NIO channel to read from.
+   * @param buf The buffer to use when reading from the nio channel.   This will be recycled.
+   */
+  def enumByteChannel(channel : IO[JByteChannel], buf : ByteBuffer) = new Enumerator[ByteBuffer, IO] {
     private def readBuf(channel : IO[JByteChannel]) : IO[(JByteChannel,Input[ByteBuffer])] = channel map { c =>
       buf.rewind()
       try {
@@ -50,72 +49,18 @@ object Channels {
       drive(i, channel)
     }
   }
-//  /** Abstract representation of an nio Channel.
-//    * TODO(jsuereth): Laziness and Promises for async?
-//    */
-//  sealed trait InputChannel[A] {
-//    def readNext : Input[A]
-//    def isOpen : Boolean
-//    def close() : Unit
-//  }
-//  sealed trait OutputChannel[A] {
-//    def write(value :A) : Unit
-//    def isOpen : Boolean
-//    def close() : Unit
-//  }
-//  def byteBufferOutputChannel(native : JByteChannel) : OutputChannel[ByteBuffer] = new OutputChannel[ByteBuffer] {
-//    def isOpen = native.isOpen
-//    def close() = native.close()
-//    def write(value : ByteBuffer) : Unit =
-//      native.write(value)
-//  }
-//  /** Wraps a ByteChannel with a sized ByteBuffer that is used to read values and feed
-//   * them to the Iteratee IO
-//   */
-//  def byteBufferInputChannel(native : JByteChannel, buf : ByteBuffer) : InputChannel[ByteBuffer] = new InputChannel[ByteBuffer] {
-//     def isOpen = native.isOpen
-//     def close() = native.close()
-//     def readNext = {
-//       buf.rewind()
-//       val size = native.read(buf)
-//       if(size >=0) {
-//         buf.flip()
-//         El(buf)
-//       } else EOF[ByteBuffer]
-//     }
-//  }
-//
-//  /** Enumerator that will drive data from a given input through an Iteratee. */
-//  implicit object ChannelEnumerator extends Enumerator[InputChannel] {
-//      def apply[E, A](channel: InputChannel[E], i: IterV[E, A]): IterV[E, A] = {
-//        @annotation.tailrec
-//        def step(io : IterV[E, A]) : IterV[E,A] =
-//          io match {
-//            case i if !channel.isOpen => i
-//            case i @ Done(_,_) =>
-//               // TODO(jsuereth): make this lazy *and* perhaps wrap entire resource in some other magikz.
-//               channel.close()
-//               i
-//            case Cont(k) =>
-//               step(k(channel.readNext))
-//          }
-//        step(i)
-//      }
-//  }
-//
-//  /** Iteratee that will write to a ByteChannel. */
-//  // TODO(jsuereth): Laziness + Safety (IO monad)?
-//  def writeToChannel[A](channel : OutputChannel[ByteBuffer]) : IterV[ByteBuffer, Unit] = {
-//    def step[A](channel : OutputChannel[ByteBuffer]) : Input[ByteBuffer] => IterV[ByteBuffer, Unit] = {
-//      case EOF() =>
-//        channel.close()
-//        Done((), EOF[ByteBuffer])
-//      case El(buf) =>
-//        channel.write(buf)
-//        Cont(step(channel))
-//      case IterV.Empty() =>
-//        Cont(step(channel))
-//    }
-//    Cont(step(channel))
-//  }
+  /** An iteratee that will write to a byte channel */
+  def writeToChannel[A](channel: IO[JByteChannel])(implicit m: Monad[IO]): Iteratee[ByteBuffer, IO, Unit] = {
+    def step[A](channel: IO[JByteChannel]): Input[ByteBuffer] => Iteratee[ByteBuffer, IO, Unit] = {
+      case EOF(None) =>
+        FlattenI(channel.map(_.close()).map(Done(_, EOF[ByteBuffer](None))))
+      case Chunk(buf) =>
+        FlattenI(channel map { c =>
+          c.write(buf)
+          Cont(i => step(m.pure(c))(i))
+        })
+      case EOF(Some(err)) => iteratees.Failure(err)
+    }
+    Cont(i => step(channel)(i))
+  }
 }
